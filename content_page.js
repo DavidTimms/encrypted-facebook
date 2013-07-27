61c864057ae0c5a2050a3d94fe49549354fbeac3
Philip McMahon <13phil13@googlemail.com>
2013-07-27 16:30:10 +0100

    messages (may need undoing!)

--- content_page.js
+++ content_page.js
@@ -1,2 +1,14 @@
 //stuff here that will be injected into the page
-console.log("This is facebook messages!");
+
+chrome.extension.sendMessage({type: "hasExtension", id: getFacebookId()}, function(response) {
+	if (response.hasExtension === true)
+	{
+		// inject buttons etc, get messages
+		console.log("yay! they have the extension");
+	}
+	else
+	{
+		// do nothing
+		console.log("Boo they don't have the extension");
+	}
+})
--- events_page.js
+++ events_page.js
@@ -1,1 +1,23 @@
-//background page 
+
+var users = new Array();
+users[0] = {id: "phil.mcmahon"}
+users[1] = {id: "david.timms.395"}
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		console.log(request);
+		if (request.type === "hasExtension")
+		{
+			for (x in users)
+				if x.id === request.id
+					sendResponse({hasExtension: true})
+		}
+		else if (request.type === "encryptedmessage")
+		{
+			//sendResponse({decryptedmessage: "message"})
+		}
+		else if (request.type === "messageToEncrypt")
+		{
+			//encrypt message!
+		}
+	})
