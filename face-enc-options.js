ca662117c5c480c648456eb88cdbdc725b7157e8
Philip McMahon <13phil13@googlemail.com>
2013-08-09 18:39:40 +0100

    Updated options page - now works with keyring. Accessor functions added for keys stored in keyring.

--- face-enc-options.js
+++ face-enc-options.js
@@ -1,14 +1,49 @@
 var facebookOpenPGPOptions = ({
-	processkey: function () {
+	userKeySet : false,
+	userFacebookEmail : "",
+	getUserPublicKey: function() {
+		if (userKeySet)
+			return openpgp.keyring.getPublicKeyForAddress(userFacebookEmail);
+		else
+			return "Key not set";
+	},
+	getUserPrivateKey: function() {
+		if (userKeySet)
+			return openpgp.keyring.getPrivateKeyForAddress(userFacebookEmail);
+		else
+			return "Key not set";
+	},
+	// this can be used with just the first parameter - checkIfUsernameInKeyList(username)
+	checkIfUsernameInKeyList: function(username, storageChecked, returnValue) {
+		if (storageChecked)
+			return returnValue;
+		chrome.storage.sync.get('friendList', function (result) {
+			if (result.friendList)
+			{
+				var friendList = result.friendList;
+				for (var i = 0; i < friendList.length; i++)
+					if (username === friendList[i].id)
+						facebookOpenPGPOptions.checkIfUser(username, true, true);
+				return facebookOpenPGPOptions.checkIfUser(username, true, false);
+			}
+		}					
+	},
+	getPublicKeyFromUserName: function(username) {
+		return openpgp.keyring.getPublicKeyForAddress(username + "@facebook.com");
+	},
+	addKey: function () {
+		var friendId = document.getElementById("friendFacebookId").value;
+		if (friendId == null || friendId.length < 1) {
+			// error!
+		}
 		var key = document.getElementById("publickey").value;
-		console.log("key: "+ key);
 		if (key == null || key =="") {
-		// do nothing
+			// do nothing
 		}
 
-		// Update status to show whether key import succesfull
+		// Update status to show whether key import succesful
 		var keyImportStatus = document.getElementById("keyImportStatus");
-
+		console.log(openpgp.keyring.publicKeys.length);
 		try {
 			openpgp.keyring.importPublicKey(key);
 		}
@@ -19,6 +54,8 @@ var facebookOpenPGPOptions = ({
 		}
 		keyImportStatus.innerHTML="Key imported.";
 
+		var keyIndex = openpgp.keyring.publicKeys.length;
+		// Store keyring
 		try {
 			openpgp.keyring.store();
 		}
@@ -26,6 +63,16 @@ var facebookOpenPGPOptions = ({
 			console.log("keyring.store failed");
 			throw exception;
 		}
+		console.log(openpgp.keyring);
+		chrome.storage.sync.get('friendList', function(result) {
+			result.friendList.push({"id" : friendId, "index" : keyIndex});
+			chrome.storage.sync.set({'friendList' : result.friendList}, function(result) {
+			 	if (chrome.runtime.lastError)
+			 		console.log("Friend id storage error");
+			 	else
+			 		facebookOpenPGPOptions.updateFriendList();
+		  	});
+		});
 	},
 	generateKey: function() {
 		var username = document.getElementById('userFacebookId').value;
@@ -36,11 +83,9 @@ var facebookOpenPGPOptions = ({
 			 "<p>Key generation failed - username at least 1 character</p>";
 			 return;
 		} 
-		else if (username.length > 8) {
-			keyId = username.substring(0,  7);
-		}
 		else
-			keyId = username;
+			keyId = username + "@facebook.com";
+		console.log(keyId);
 		// store username in chrome sync storage
 		chrome.storage.sync.set({"facebookUsername" : username}, function(result) {
 			if (chrome.runtime.lastError)
@@ -56,46 +101,121 @@ var facebookOpenPGPOptions = ({
 		}
 		openpgp.keyring.importPrivateKey(newKey.privateKeyArmored);
 		openpgp.keyring.importPublicKey(newKey.publicKeyArmored);
+		openpgp.keyring.store();
+		console.log(openpgp.keyring);
 		document.getElementById("userPublicKey").innerHTML = "<p>" + newKey.publicKeyArmored + "</p>";
-		facebookOpenPGPOptions.checkSetUp();
+		facebookOpenPGPOptions.loadUserDetailsAndInitFriendList();
 	},
-	deletekey: function() {
-		chrome.storage.sync.remove("facebookUsername", function(result) {
+	deleteAllKeysInKeyring: function() {
+		console.log(openpgp.keyring.publicKeys.length);
+		while (openpgp.keyring.publicKeys.length > 0)
+			openpgp.keyring.removePublicKey(0);
+		while (openpgp.keyring.privateKeys.length > 0)
+			openpgp.keyring.removePrivateKey(0);
+		console.log(openpgp.keyring.publicKeys.length);
+		console.log(openpgp.keyring);
+		chrome.storage.sync.remove("friendList", function(result) {
 			if (chrome.runtime.lastError)
-				console.log("Key delete error");
+				console.log("Key list delete error");
+			else
+			{
+				openpgp.keyring.store();
+				chrome.storage.sync.remove("facebookUsername", function(result) {
+				if (chrome.runtime.lastError)
+					console.log("Key delete error");
+				else
+				{
+					facebookOpenPGPOptions.updateFriendList();
+					facebookOpenPGPOptions.loadUserDetailsAndInitFriendList();
+				}
+				});
+			}
 		});
-		facebookOpenPGPOptions.checkSetUp();
 	},
-	createNewKeyring: function() {
-		//var keyring = new openpgp_keyring;
+	deleteFriend: function(e) {
+		console.log(openpgp.keyring.publicKeys.length)
+		var indexToDelete = e.srcElement.friendListIndex
+		chrome.storage.sync.get('friendList', function(result) {
+			var removeAt = result.friendList[indexToDelete].index -1;
+			console.log(removeAt);
+			openpgp.keyring.removePublicKey(removeAt);
+			for (var i = indexToDelete + 1; i < result.friendList.length; i++)
+				result.friendList[i].index--;
+			console.log(openpgp.keyring.publicKeys.length)
+			result.friendList.splice(indexToDelete, 1);
+			chrome.storage.sync.set({'friendList' : result.friendList}, function(result) {
+				if (chrome.runtime.lastError)
+					console.log("Friend delete error");
+				else
+					facebookOpenPGPOptions.updateFriendList();
+			});
+		});
 	},
-	checkSetUp: function() {
+	updateFriendList: function() {
+		chrome.storage.sync.get('friendList', function(result) {
+			if (result.friendList)
+			{
+				var friendList = result.friendList;
+				document.getElementById("friendListElements").innerHTML = "";
+				for (var i = 0; i < friendList.length; i++)
+				{
+					var li = document.createElement("li");
+					console.log(friendList[i].id);
+					li.appendChild(document.createTextNode(friendList[i].id + friendList[i].index))
+					var deleteFriendButton = document.createElement("button");
+					deleteFriendButton.id = "deleteFriendButton";
+					deleteFriendButton.type = "submit";
+					deleteFriendButton.value = "Delete Friend";
+					deleteFriendButton.friendListIndex = i;
+					deleteFriendButton.appendChild(document.createTextNode("Delete Friend"));
+					deleteFriendButton.addEventListener('click', facebookOpenPGPOptions.deleteFriend);
+					li.appendChild(deleteFriendButton);
+					document.getElementById("friendListElements").appendChild(li);
+				}
+			}
+		});
+	},
+	loadUserDetailsAndInitFriendList: function() {
 		chrome.storage.sync.get('facebookUsername', function(result) {
 			if (result.facebookUsername)
 			{
+				loadUserDetailsAndInitFriendList = true;
+				userFacebookEmail = result.facebookUsername + "@facebook.com";
 				document.getElementById('generateKey').style.display = 'none';
 				document.getElementById('userKeys').style.display = 'inline';
 				document.getElementById('keyInfoText').innerHTML =
 				 "<p> You have a key set up for username " + result.facebookUsername + ": </p>";
-				 //document.getElementById("userPublicKey").innerHTML = "";
-				 console.log(openpgp.keyring.getPublicKeysForKeyId(result.facebookUsername.substring(0,7)));
+				document.getElementById("userPublicKey").innerHTML = "<p>" +
+				 openpgp.keyring.getPublicKeyForAddress(userFacebookEmail)[0].armored + "</p>";
 			} 
 			else {
 				document.getElementById('userKeys').style.display = 'none';
 				document.getElementById('generateKey').style.display = 'inline';
 			}
-			console.log(result);
-			//console.log(chrome.runtime.lastError);
+			if(chrome.runtime.lastError)
+				console.log("Username check error");
+		});
+		chrome.storage.sync.get('friendList', function(result) {
+			if (result.friendList)
+				console.log("Friend list exists:" + result);
+			else
+			{
+				console.log("Creating friend list");
+				chrome.storage.sync.set({"friendList" : new Array()});
+			}
 		});
 	},
 	init: function() {
-		this.checkSetUp();
+		this.loadUserDetailsAndInitFriendList();
 		openpgp.init();
-		//keyring = new openpgp_keyring();
-		console.log("Initialising options js");
-		document.querySelector('#saveKey').addEventListener('click', this.processkey);
+		
+		// Setup buttons
+		document.querySelector('#saveKey').addEventListener('click', this.addKey);
 		document.querySelector('#generateKeyButton').addEventListener('click', this.generateKey);
 		document.querySelector('#deleteUserKey').addEventListener('click', this.deletekey);
+		document.querySelector('#deleteAllKeys').addEventListener('click', this.deleteAllKeysInKeyring);
+		
+		this.updateFriendList();
 		return this;
 	}
 }).init();
